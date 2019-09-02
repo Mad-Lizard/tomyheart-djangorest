ebc6db30a1e122a8a266b3eb09351da66ed2f8dd
Mad-Lizard <annamavka@gmail.com>
2019-09-02 13:26:27 +0300

    Djangorestframework tutorials done

--- recovery/urls.py
+++ recovery/urls.py
@@ -1,30 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from recovery import views
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 
-post_list = views.PostViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-    })
-post_detail = views.PostViewSet.as_view({
-    'get': 'retreive',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-user_list = views.UserViewSet.as_view({
-    'get': 'list'
-})
-user_detail = views.UserViewSet.as_view({
-    'get': 'retreive'
-})
+router = DefaultRouter()
+router.register(r'api/posts', views.PostViewSet)
+router.register(r'api/users', views.UserViewSet)
 
 urlpatterns = [
-    path('api', views.api_root),
-    path('posts/', post_list, name='post-list'),
-    path('post/<int:pk>/', post_detail, name='post-detail'),
-    path('users/', user_list, name='user-list'),
-    path('user/<int:pk>/', user_detail, name='user-detail'),
+    path('', include(router.urls)),
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+#urlpatterns = format_suffix_patterns(urlpatterns)
